4a6ff01f28768613a545b05535e417df87a8af07
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2024-10-22 15:09:21 -0700

    Tryign

--- attempt.py
+++ attempt.py
@@ -2,6 +2,8 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
 import warnings
 import numpy as np
 import torch
+from tqdm import tqdm
+import pickle
 
 
 def set_tracking_config(config, track_layers=[2, 15, 30], track_mlp=True, track_attention=True):
@@ -49,17 +51,30 @@ def read_hidden_states(probe_hidden_output):
     layers, keys, input_length, batch_size = details
     ret = {}
     for layer in layers:
-        ret[layer] = {}
+        ret[f"layer_{layer}"] = {}
         for key in keys:
-            ret[layer][key] = [probe_hidden_output[0][layer][key]]
+            ret[f"layer_{layer}"][key] = [probe_hidden_output[0][layer][key]]
     for hidden_output in probe_hidden_output[1:]:
          for layer in layers:
               for key in keys:
-                   ret[layer][key].append(hidden_output[layer][key])
+                   ret[f"layer_{layer}"][key].append(hidden_output[layer][key])
     for layer in layers:
          for key in keys:
-              ret[layer][key] = torch.cat(ret[layer][key], dim=1)
+              ret[f"layer_{layer}"][key] = torch.cat(ret[f"layer_{layer}"][key], dim=1)
     return ret
+
+def merge_batches(main_hidden_states, secondary_hidden_states):
+    assert main_hidden_states is not None and secondary_hidden_states is not None
+    for layer in main_hidden_states.keys():
+        for key in main_hidden_states[layer].keys():
+            main_hidden_states[layer][key] = torch.cat([main_hidden_states[layer][key], secondary_hidden_states[layer][key]], dim=0)
+    return main_hidden_states
+     
+
+def to_numpy(hidden_states):
+     for layer in hidden_states.keys():
+          for key in hidden_states[layer].keys():
+               hidden_states[layer][key] = hidden_states[layer][key].numpy()
 
 
 name = "meta-llama/Llama-3.1-8B-Instruct"
@@ -72,13 +87,26 @@ set_tracking_config(config, track_layers=[2, 15, 30], track_mlp=True, track_atte
 model = AutoModelForCausalLM.from_pretrained(name, config=config)
 model.config.pad_token_id = model.config.eos_token_id
 
-prompt = "What was Einsteins first name? "
+prompts = ["What was Einsteins first name? ", "What is the capital of Paris? "]
+inputs = tokenizer(prompts, return_tensors="pt").to(model.device)
+hidden_states = None
+for i in tqdm(range(5)): 
+    output = model.generate(**inputs, max_new_tokens=3)
+    if hidden_states is None: 
+        hidden_states = read_hidden_states(model.probe_hidden_output)
+    else:
+        hidden_states = merge_batches(hidden_states, read_hidden_states(model.probe_hidden_output))
+    model.probe_reset_hidden_output()
 
+data_pkl = "hidden_states_torch.pkl"
+with open(data_pkl, "wb") as f:
+    pickle.dump(hidden_states, f)
+to_numpy(hidden_states)
+data_np_pkl = "hidden_states_np.pkl"
+with open(data_np_pkl, "wb") as f:
+    pickle.dump(hidden_states, f)
+data_npz = "hidden_states.npz"
+np.savez(data_npz, **hidden_states)
+print("Saved to all formats")
 
-
-
-inputs = tokenizer(prompt, return_tensors="pt")
-output = model.generate(**inputs, max_new_tokens=10)
-hidden_states = read_hidden_states(model.probe_hidden_output)
-print(tokenizer.decode(output[0], skip_special_tokens=True))
 
