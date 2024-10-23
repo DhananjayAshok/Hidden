3529e8f0755cbdefd73523e670b5dfbe4c64e69e
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2024-10-23 15:28:44 -0700

    bug

--- save_inference.py
+++ save_inference.py
@@ -12,12 +12,14 @@ import os
 @click.option("--output_csv_path", type=str, required=True)
 @click.option("--output_hidden_path", type=str, required=True)
 @click.option("--max_new_tokens", type=int, default=10)
-@click.option("--stop_string", type=str, default="[STOP]")
-@click.option("--remove_stop_string", type=bool, default=True)
+@click.option("--stop_strings", type=str, default="[STOP]")
+@click.option("--remove_stop_strings", type=bool, default=True)
 @click.option("--track_layers", type=str, default="2,15,30")
 @click.option("--track_mlp", type=bool, default=True)
 @click.option("--track_attention", type=bool, default=True)
-def main(model_name, data_path, output_csv_path, output_hidden_path, max_new_tokens, stop_string, remove_stop_string, track_layers, track_mlp, track_attention):
+def main(model_name, data_path, output_csv_path, output_hidden_path, max_new_tokens, stop_strings, remove_stop_strings, track_layers, track_mlp, track_attention):
+    track_layers = [int(x) for x in track_layers.split(",")]
+    stop_strings = stop_strings.split(",")
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     tokenizer.pad_token = tokenizer.eos_token
     config = AutoConfig.from_pretrained(model_name)
@@ -30,14 +32,15 @@ def main(model_name, data_path, output_csv_path, output_hidden_path, max_new_tok
         prompt = data_df.loc[i, "text"]
         inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
         input_length = inputs["input_ids"].shape[1]
-        output = model.generate(**inputs, max_new_tokens=max_new_tokens, stop_strings=[stop_string], pad_token_id=tokenizer.eos_token_id)
+        output = model.generate(**inputs, max_new_tokens=max_new_tokens, stop_strings=stop_strings, pad_token_id=tokenizer.eos_token_id)
         hidden_states = read_hidden_states(model.probe_hidden_output)
         model.probe_reset_hidden_output()
         hidden_states_list.append(hidden_states)
         output_only = output[0, input_length:]
         out = tokenizer.decode(output_only, skip_special_tokens=True)
-        if remove_stop_string:
-            out = out.replace(stop_string, "")
+        if remove_stop_strings:
+            for stop_string in stop_strings:
+                out = out.replace(stop_string, "")
         data_df.loc[i, "output"] = out
     for output_path in [output_csv_path, output_hidden_path]:
         if os.path.dirname(output_path) != "":
