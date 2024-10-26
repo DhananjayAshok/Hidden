fd1763921f46055f21cbea97c683eedecac20922
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2024-10-26 10:10:57 -0700

    Modeling added

---
+++ end_to_end_setup.sh
@@ -0,0 +1,11 @@
+git submodule init
+git submodule update
+conda create -n hidden python=3.10
+conda activate hidden
+conda install pytorch pytorch-cuda=12.1 -c pytorch -c nvidia -y
+cd transformers
+pip install -e .
+cd ..
+pip install click, gdown, datasets, accelerate
+bash get_data.sh
+python data.py 
---
+++ modeling.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import os
+import pickle
+
+
+layer_keys = ['layer_2', 'layer_15', 'layer_30']
+hidden_keys = ["attention", "mlp"]
+dataset = "qnota"
+hidden_states_dir = "results/unanswerable/"
+
+def get_pickles(files):
+    subfiles = []
+    for file in files:
+        if ".pkl" in file:
+            subfiles.append(int(file.split(".")[0]))
+        else:
+            pass
+    return subfiles
+
+
+train_files = os.listdir(f"{hidden_states_dir}/train/{dataset}/")
+train_files = get_pickles(train_files)
+test_files = os.listdir(f"{hidden_states_dir}/test/{dataset}/")
+test_files = get_pickles(test_files)
+train_files = sorted(train_files)
+test_files = sorted(test_files)
+
+train_array = []
+for file in train_files:
+    internal_array = []
+    with open(f"{hidden_states_dir}/train/{dataset}/{file}.pkl", "rb") as f:
+        hidden_states = pickle.load(f)
+    for layer_key in layer_keys:
+        for hidden_key in hidden_keys:
+            for hidden_state in hidden_states[layer_key][hidden_key]:
+                internal_array.append(hidden_state)
+    train_array.append(internal_array)
+train_array = np.array(train_array)
+
