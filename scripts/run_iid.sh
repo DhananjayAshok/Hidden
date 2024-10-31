b7435dd1b9c65c935785be47404e8b2eb57b409d
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2024-10-31 13:48:29 -0700

    Added bugfix

--- scripts/run_iid.sh
+++ scripts/run_iid.sh
@@ -8,10 +8,8 @@ for task in "${tasks[@]}"
 do
     for dataset in "${datasets[@]}"
     do
+        echo "Running IID for $task $dataset"
         prediction_dir=$RESULTS_DIR/$task/predictions/$dataset/
-        python run_iid.py --task $task --dataset $dataset --prediction_dir $prediction_dir --random_sample_train $random_sample_train --random_sample_test $random_sample_test
+        python iid_modeling.py --task $task --dataset $dataset --prediction_dir $prediction_dir --random_sample_train $random_sample_train --random_sample_test $random_sample_test
     done
 done
-
-prediction_dir=$RESULTS_DIR/$task/predictions/$dataset/
-python run_iid.py --task $task --dataset $dataset --prediction_dir $prediction_dir --random_sample_train $random_sample_train --random_sample_test $random_sample_test
