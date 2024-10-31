62ab715eaf79a79b2e36e2142d9025de6e1bcda4
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2024-10-31 16:39:29 -0700

    tryign

--- scripts/run_iid.sh
+++ scripts/run_iid.sh
@@ -1,15 +1,18 @@
-tasks=("confidence")
-datasets=("mmlu")
+declare -A tasks_and_datasets=(
+    ["confidence"]="mmlu healthver"
+)
 random_sample_train=1000
 random_sample_test=1000
 
 source proj_params.sh
-for task in "${tasks[@]}"
+
+for task in "${!tasks_and_datasets[@]}"
 do
-    for dataset in "${datasets[@]}"
+    datasets=${tasks_and_datasets[$task]}
+    for dataset in $datasets
     do
         echo "Running IID for $task $dataset"
         prediction_dir=$RESULTS_DIR/$task/predictions/$dataset/
-        python iid_modeling.py --task $task --dataset $dataset --prediction_dir $prediction_dir --random_sample_train $random_sample_train --random_sample_test $random_sample_test
+        echo python iid_modeling.py --task $task --dataset $dataset --prediction_dir $prediction_dir --random_sample_train $random_sample_train --random_sample_test $random_sample_test
     done
 done
