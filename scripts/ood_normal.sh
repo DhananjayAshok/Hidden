e2738bb0a3348633e672cfcbcb100cbc36921d9f
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2024-11-18 17:38:07 -0800

    Shift

--- scripts/ood_normal.sh
+++ scripts/ood_normal.sh
@@ -10,6 +10,9 @@ source proj_params.sh
 random_sample_test_per=200000
 random_seed=42
 model_save_name="${model_name#*/}"
+only_attention=False
+only_mlp=True
+only_layer=15
 
 
 for ood_config_file in "${ood_config_files[@]}"
@@ -28,5 +31,5 @@ do
     done
     logfile=$LOG_DIR/probe_ood/$model_save_name/$run_name/$model_kind/train_${random_sample_train_per}-seed_${random_seed}.log
     mkdir -p $(dirname $logfile)
-    python ood_modeling.py --run_name_base $run_name_base $taskdata --model_save_name $model_save_name --random_sample_train_per $random_sample_train_per --random_sample_test_per $random_sample_test_per --random_seed $random_seed --strict_w_dataset $strict_w_dataset --strict_w_task $strict_w_task --mix_iid_n $mix_iid_n --model_kind $model_kind > $logfile
+    python ood_modeling.py --run_name_base $run_name_base $taskdata --model_save_name $model_save_name --random_sample_train_per $random_sample_train_per --random_sample_test_per $random_sample_test_per --random_seed $random_seed --strict_w_dataset $strict_w_dataset --strict_w_task $strict_w_task --mix_iid_n $mix_iid_n --model_kind $model_kind --only_attention $only_attention --only_mlp $only_mlp --only_layer $only_layer > $logfile
 done
